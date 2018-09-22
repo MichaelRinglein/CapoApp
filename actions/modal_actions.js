5977eea30b5b6bd7fb1698a7b9d1ef9f00e4f406
MichaelRinglein <michaelringlein@gmail.com>
2018-09-22 21:06:58 +0300

    Removed Facebook-Ads, build Version 1.2.0

--- actions/modal_actions.js
+++ actions/modal_actions.js
@@ -1,5 +1,3 @@
-import { FacebookAds } from 'expo';
-
 import {
     OPEN_CHORDS_MODAL,
     CLOSE_CHORDS_MODAL
@@ -12,14 +10,7 @@ export const openChordsModal = () => ({
 
 let expirationDate;
   
-export const closeChordsModal = () => {
-    if(!expirationDate || new Date() > expirationDate) {
-        expirationDate = new Date(
-            new Date().getTime() + (60 * 1000 * 2)
-        );
-        FacebookAds.InterstitialAdManager.showAd('976225125859901_976228469192900')
-        .then(console.log('Int Add shown'))
-        .catch(err => console.log('Interstitial Ad Error' , err));
-    }    
-    return { type: CLOSE_CHORDS_MODAL, payload: false };
-};
+export const closeChordsModal = () => ({
+    type: CLOSE_CHORDS_MODAL, 
+    payload: false
+});
